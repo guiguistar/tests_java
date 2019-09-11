0d871509138aa2c8d8610a798d6cfac92c40b321
Guillaume Roux <guiguionthenet@free.fr>
2019-09-11 17:00:19 +0200

    Séparation des classes Normale2D et KDistribution

---
+++ KMoyennes/Normale2D.java
@@ -0,0 +1,33 @@
+import java.util.Random;
+
+class Normale2D {
+	Random rnd = new Random();
+	double muX, muY, sigmaX, sigmaY;
+	
+	Normale2D(double muX, double muY, double sigmaX, double sigmaY) {
+		this.muX = muX;
+		this.muY = muY;
+		this.sigmaX = sigmaX;
+		this.sigmaY = sigmaY;
+	}
+	
+	public void setMuX(double muX) {
+		this.muX = muX;
+	};
+	public void setMuY(double muY) {
+		this.muY = muY;
+	};
+	public void setSigmaX(double sigmaX) {
+		this.sigmaX = sigmaX;
+	};
+	public void setSigmaY(double sigmaY) {
+		this.sigmaY = sigmaY;
+	};
+
+	public Point realisation() {
+		double x = this.muX + this.sigmaX * this.rnd.nextGaussian();
+		double y = this.muY + this.sigmaY * this.rnd.nextGaussian();
+		
+		return new Point(x, y);
+	}
+}
